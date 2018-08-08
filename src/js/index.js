0e511da095fc4f5f0fb9d7fe68de76927ea3ef59
Elissa <syue1018@hotmail.com>
2018-08-08 18:28:57 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -14,6 +14,16 @@ new Vue({
       "icon": "./images/icon/portal@2x.png",
     },
     isOpenChainHeight: false,
+    onePhotos: [],
+    isOpenPop: false,
+    isOpenPopMore: false,
+    morePhotos: [
+      "./images/index/dom_01.png",
+      "./images/index/dom_02.png",
+      "./images/index/dom_03.png",
+      "./images/index/dom_04.png",
+      "./images/index/dom_05.png",
+    ]
   },
   methods: {
     toggleMenuFn() {
@@ -41,11 +51,48 @@ new Vue({
         "title": "PORTAL",
         "icon": "./images/icon/portal@2x.png",
       }
-    }
+    },
+    openModal: function(name) {
+      this.isOpenPop = true;
+      let pic = "";
+      switch(name){
+        case "cli":
+          pic = `./images/index/cli.png`;
+          break;
+        case "deploy":
+          pic = `./images/index/deploy.png`;
+          break;
+        case "toolkits":
+          pic = `./images/index/toolkits.png`;
+          break;
+      }
+      this.onePhotos.push(pic);
+    },
+    closeModal: function() {
+      this.isOpenPop = false;
+      this.onePhotos = [];
+    },
+    openMoreModal: function() {
+      this.isOpenPopMore = true;
+    },
+    closeModalMore: function() {
+      this.isOpenPopMore = false;
+      this.morePhotos = [];
+    },
   },
   mounted() {
     AOS.init();
     window.addEventListener('scroll', this.scrollFn);
     axios.get("assets/json/blockchain.json").then(this.blockchainData).catch(err=> console.log(err));
+    this.swiper = new Swiper('.swiper-container', {
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'progressbar',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
   }
 });
